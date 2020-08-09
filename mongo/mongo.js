d6e9135a516cf34c5c1337096a31c706d0a295d0
unknown <lironbard@gmail.com>
2020-08-09 19:02:32 +0300

    090820

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://lironbard:*****@cluster0.v8egm.gcp.mongodb.net/toysliron?authSource=admin&replicaSet=atlas-f4rmk9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', { useNewUrlParser: true,useUnifiedTopology:true });
+mongoose.connect('mongodb+srv://lironbard:igiigi47@cluster0.v8egm.gcp.mongodb.net/toys_liron?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
